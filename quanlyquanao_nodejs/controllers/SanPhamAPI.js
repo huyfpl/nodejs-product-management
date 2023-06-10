784440bbb4e0f8df53b235359c02f0820b90e15a
Huyfpl <94282645+huyfpl@users.noreply.github.com>
2023-06-10 11:11:06 +0700

    update api

--- quanlyquanao_nodejs/controllers/SanPhamAPI.js
+++ quanlyquanao_nodejs/controllers/SanPhamAPI.js
@@ -1,17 +1,45 @@
-module.exports.get_list_api = (req, res) => {
-  console.log(req+"huy")
-    req.getConnection((err, conn) => {
+module.exports.get_list_api_sanpham = (req, res) => {
+  console.log(req + "huy")
+  req.getConnection((err, conn) => {
+    if (err) {
+      res.json(err);
+    } else {
+      conn.query('SELECT * FROM products', (err, products) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json({ products: products });
+        }
+      });
+    }
+  });
+};
+module.exports.get_list_api_user = (req, res) => {
+  console.log(req + "huy")
+  req.getConnection((err, conn) => {
+    if (err) {
+      res.json(err);
+    } else {
+      conn.query('SELECT * FROM users', (err, users) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json({ products: users });
+        }
+      });
+    }
+  });
+};
+module.exports.post_list_api_user = (req, res) => {
+  const {tentaikhoan,pass} = req.body;
+  const data = { tentaikhoan, pass }; 
+  req.getConnection((err, conn) => {
+    const query = conn.query('INSERT INTO users SET ?', data, (err, user) => {
       if (err) {
         res.json(err);
-      } else {
-        conn.query('SELECT * FROM products', (err, products) => {
-          if (err) {
-            res.json(err);
-          } else {
-            res.json({ products: products });
-          }
-        });
       }
+      console.log(user);
+      console.log(data);
     });
-  };
-  
+  });
+};
--- quanlyquanao_nodejs/routers/api.js
+++ quanlyquanao_nodejs/routers/api.js
@@ -4,6 +4,8 @@ const express=require('express');
 const router= express.Router();
 
 
-router.get('/listsanpham', sanpham_api.get_list_api);
+router.get('/listsanpham', sanpham_api.get_list_api_sanpham);
+router.get('/listuser', sanpham_api.get_list_api_user);
+router.post('/register-user', sanpham_api.post_list_api_user);
 
 module.exports = router;
