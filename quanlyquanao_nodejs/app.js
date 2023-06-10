572de8adb3e241a78b155571fb144c719d11f470
Huyfpl <94282645+huyfpl@users.noreply.github.com>
2023-06-10 20:27:30 +0700

    tạo danh mục cho từng sản phẩm

--- quanlyquanao_nodejs/app.js
+++ quanlyquanao_nodejs/app.js
@@ -8,7 +8,8 @@ let myConnection = require('express-myconnection');
 let morgan = require('morgan');
 const app = express();
 const apiSanPham =require('./routers/api')
-
+const Handlebars = require('handlebars');
+const handlebarsHelpers = require('handlebars-helpers');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -26,13 +27,13 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql, {
     host: 'sql.freedb.tech',
     user: 'freedb_sanpham_test',
-    password: '%rvhFjK!9fSec8p',
+    password: '@GbTmU&g9mPeZaM',
     port: 3306,
     database: 'freedb_test_sanpham'
     ,
-    connectTimeout:10000
+    connectTimeout:100000
 }, 'single'));
-
+Handlebars.registerHelper('isEqual', handlebarsHelpers.comparison().eq);
 
 app.use(Router);
 app.use('/api',apiSanPham)
@@ -40,6 +41,6 @@ const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 // Khởi động server
-app.listen(app.get('port'), '192.168.1.12', () => {
+app.listen(app.get('port'), '192.168.0.102', () => {
     console.log(`Server đang khởi động ở cổng ${app.get('port')}`);
   });
