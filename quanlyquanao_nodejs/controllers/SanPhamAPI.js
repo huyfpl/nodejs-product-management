4497a6d160f762659a5526683778974f2a5e8785
Huyfpl <94282645+huyfpl@users.noreply.github.com>
2023-06-14 17:28:50 +0700

    update api gio hang

--- quanlyquanao_nodejs/controllers/SanPhamAPI.js
+++ quanlyquanao_nodejs/controllers/SanPhamAPI.js
@@ -79,6 +79,7 @@ module.exports.get_list_api_danhmuc = (req, res) => {
     }
   });
 };
+
 // lấy danh sách theo danh mục
 module.exports.get_list_api_sanpham_id_danhmuc = (req, res) => {
   const { id } = req.params;
@@ -93,3 +94,74 @@ module.exports.get_list_api_sanpham_id_danhmuc = (req, res) => {
     });
   });
 };
+// lấy giỏ hàng theo id user
+module.exports.get_list_api_giohang = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    if (err) throw err;
+    const query = `
+    SELECT products.tenSP, products.giaBan, products.anhSP, danhmuc.ten_danhmuc
+    FROM giohang
+    JOIN products ON giohang.idSP = products.idSP
+    JOIN danhmuc ON products.id_danhmuc = danhmuc.id_danhmuc
+    JOIN users ON giohang.id = users.id
+    WHERE users.id = ?;
+    `;
+    conn.query(query, [id], (err, products) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json({ products: products });
+      }
+    });
+  });
+};
+// kiểm tra tồn tại trong giỏ hàng hay chưa
+module.exports.add_to_giohang = (req, res) => {
+  const { id, idSP } = req.params;
+  req.getConnection((err, conn) => {
+    if (err) throw err;
+    const checkQuery = `
+    SELECT soluong
+    FROM giohang
+    WHERE id = ? AND idSP = ?;
+    `;
+    conn.query(checkQuery, [id, idSP], (err, results) => {
+      if (err) {
+        res.json(err);
+      } else {
+        if (results.length > 0) {
+          // Sản phẩm đã có trong giỏ hàng, tăng số lượng lên 1
+          const updateQuery = `
+          UPDATE giohang
+          SET soluong = soluong + 1
+          WHERE id = ? AND idSP = ?;
+          `;
+          conn.query(updateQuery, [id, idSP], (err, updateResult) => {
+            if (err) {
+              res.json(err);
+            } else {
+              res.json({ message: 'Số lượng sản phẩm đã được tăng.' });
+            }
+          });
+        } else {
+          // Sản phẩm chưa có trong giỏ hàng, thêm mới vào giỏ hàng với số lượng là 1
+          const insertQuery = `
+          INSERT INTO giohang (id, idSP, soluong)
+          VALUES (?, ?, 1);
+          `;
+          conn.query(insertQuery, [id, idSP], (err, insertResult) => {
+            if (err) {
+              res.json(err);
+            } else {
+              res.json({ message: 'Sản phẩm đã được thêm vào giỏ hàng.' });
+            }
+          });
+        }
+      }
+    });
+  });
+};
+
+// kiểm tra có trong giỏ hàng thì tăng số lượng
+
--- quanlyquanao_nodejs/routers/api.js
+++ quanlyquanao_nodejs/routers/api.js
@@ -10,5 +10,6 @@ router.post('/register-user', sanpham_api.post_list_api_user);
 router.get('/danhmuc', sanpham_api.get_list_api_danhmuc);
 router.get('/listsanpham-danhmuc/:id', sanpham_api.get_list_api_sanpham_id_danhmuc);
 router.get('/listuser-id/:id', sanpham_api.get_list_api_user_id);
+router.get('/giohang/:id', sanpham_api.get_list_api_giohang);
 
 module.exports = router;
